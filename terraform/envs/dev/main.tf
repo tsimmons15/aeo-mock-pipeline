34b4febb3495a656ff799ef8fd95d9fa83100c2f
Timothy Simmons <derRiese15@gmail.com>
2026-03-04 16:42:25 -0500

    Final testing of the current iteration of the terraform. Need to set up local ADC to continue.

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,16 +1,37 @@
+terraform {
+  backend "gcs" {
+    bucket = "aeo-state-tf-dev"
+    prefix = "state"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0"
+    }
+  }
+}
+
 #####################################################################
 # The mock data generator
 #####################################################################
 module "aeo_mock_generator" {
-  source = "../../modules/cloud_run_functions/mock_generator"
-  # The variables defined in the modules/cloud_run_functions/mock_generator module
+  source = "../../modules/cloud-run-functions/mock-generator"
+  # The variables defined in the modules/cloud-run-functions/mock-generator module
   project_id            = var.project_id
   region                = var.region
   function_name         = var.generator_name
   sa_name               = var.sa_name
 
-  pubsub_topic_id       = module.pubsub.topic_id
-  raw_bucket_name       = module.storage.raw_landing
+  pubsub_topic_id       = module.pubsub.pubsub_topic
+  raw_bucket_name       = module.storage.raw_landing_name
 }
 
 #####################################################################
@@ -21,4 +42,17 @@ module "storage" {
 
     project_id = var.project_id
     region = var.region
+    storage_class = var.storage_class
+    retention_period = var.retention_period
+    raw_landing_name = var.raw_bucket_name
+}
+
+#####################################################################
+# The PubSub module to define creating the PubSub topics/subscriptions
+#####################################################################
+module "pubsub" {
+  source = "../../modules/pubsub"
+
+  pubsub_topic_name = var.pubsub_topic_name
+  pubsub_subscriber_name = var.pubsub_subscriber_name
 }
